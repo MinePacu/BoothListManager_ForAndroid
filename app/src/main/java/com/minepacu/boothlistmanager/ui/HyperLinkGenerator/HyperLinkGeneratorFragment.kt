e0c665c43d0f92c305d8ea9a90a1585ff10f5040
MinePacu <subin11@hotmail.co.kr>
2024-04-01 22:33:31 +0900

    쓰지 않는 Import문 정리
    - HyperLinkGeneratorFragment.kt: 쓰지 않는 Import문 정리

--- app/src/main/java/com/minepacu/boothlistmanager/ui/HyperLinkGenerator/HyperLinkGeneratorFragment.kt
+++ app/src/main/java/com/minepacu/boothlistmanager/ui/HyperLinkGenerator/HyperLinkGeneratorFragment.kt
@@ -1,6 +1,5 @@
 package com.minepacu.boothlistmanager.ui.HyperLinkGenerator
 
-import android.app.DatePickerDialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
