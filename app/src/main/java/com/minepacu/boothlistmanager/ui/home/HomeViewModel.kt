f616179d9f2d4b32082744f63d7fac75faa46205
MinePacu <subin11@hotmail.co.kr>
2024-04-05 22:09:15 +0900

    개선사항 #25 를 반영하기 위한 로직 코드 추가 및 기존 코드 개선

--- app/src/main/java/com/minepacu/boothlistmanager/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/minepacu/boothlistmanager/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.minepacu.boothlistmanager.ui.home
 
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.lifecycle.LiveData
@@ -15,35 +16,75 @@ import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
+    // Google API Status TextView
     private var _text_ServiceConnectionStatus = MutableLiveData<String>().apply {
         value = "서비스 상태"
     }
     var text_ServiceConnectionStatus: LiveData<String> = _text_ServiceConnectionStatus
-
-    private var _text_sheetTitle = MutableLiveData<String>().apply {
-        value = ""
-    }
-    var text_sheetTitle: LiveData<String> = _text_sheetTitle
-
-    private var _text_worksheetTitle = MutableLiveData<String>().apply {
-        value = ""
-    }
-    var text_worksheetTitle: LiveData<String> = _text_worksheetTitle
 
     private var _image_login = MutableLiveData<Int>().apply {
         value = 0
     }
     var image_login: LiveData<Int> = _image_login
 
+    // =============================================================================================
+
+    // SheetTitle
+    private var _text_sheetTitle = MutableLiveData<String>().apply {
+        value = ""
+    }
+    var text_sheetTitle: LiveData<String> = _text_sheetTitle
+
     private var _image_loadedsheetInfo = MutableLiveData<Int>().apply {
         value = 0
     }
     var image_loadedsheetInfo: LiveData<Int> = _image_loadedsheetInfo
 
+    // =============================================================================================
+
+    private var _text_worksheetTitle = MutableLiveData<String>().apply {
+        value = ""
+    }
+    var text_worksheetTitle: LiveData<String> = _text_worksheetTitle
+
+
     private var _image_loadedworksheetInfo = MutableLiveData<Int>().apply {
         value = 0
     }
     var image_loadedworksheetInfo: LiveData<Int> = _image_loadedworksheetInfo
+
+    // =============================================================================================
+
+    private var _text_preorder_info_sheetTitle = MutableLiveData<String>().apply {
+        value = ""
+    }
+    var text_preorder_info_sheetTitle: LiveData<String> = _text_preorder_info_sheetTitle
+
+    private var _text_mail_order_sheetTitle = MutableLiveData<String>().apply {
+        value = ""
+    }
+    var text_mail_order_sheetTitle: LiveData<String> = _text_mail_order_sheetTitle
+
+    private var _text_grasping_demand_sheetTitle = MutableLiveData<String>().apply {
+        value = ""
+    }
+    var text_grasping_demand_sheetTitle: LiveData<String> = _text_grasping_demand_sheetTitle
+
+
+    private var _image_preorder_info_sheet = MutableLiveData<Int>().apply {
+        value = 0
+    }
+    var image_preorder_info_sheet: LiveData<Int> = _image_preorder_info_sheet
+
+    private var _image_mail_order_sheet = MutableLiveData<Int>().apply() {
+        value = 0
+    }
+    var image_mail_order_sheet: LiveData<Int> = _image_mail_order_sheet
+
+    private var _image_grasping_demand_sheet = MutableLiveData<Int>().apply {
+        value = 0
+    }
+    var image_grasping_demand_sheet: LiveData<Int> = _image_grasping_demand_sheet
 
     var isLoginToGoogleAPI = false
     var isLoadedSheetId = false
@@ -74,10 +115,10 @@ class HomeViewModel : ViewModel() {
         }
     }
 
-    fun getSheet(view : View, sheetId: String, sheetNumber: Int) {
+    fun getSheet(view : View, sheetId: String, preorder_SheetNumber: Int, mail_order_SheetNumber: Int, grasping_Demand_SheetNumber: Int) {
         viewModelScope.launch {
             val result2 = try {
-                PythonClass.getSheet_WorkSheet(sheetId, sheetNumber)
+                PythonClass.getSheet(sheetId)
             } catch (e : PyException) {
                 Result.Error(Exception(e.message))
             }
@@ -97,34 +138,96 @@ class HomeViewModel : ViewModel() {
                 }
             }
 
-            getWorksheetTitle(view, isLoadedSheetId)
+            getWorksheetTitle(view, sheetId, preorder_SheetNumber, mail_order_SheetNumber, grasping_Demand_SheetNumber, isLoadedSheetId)
         }
     }
 
-    fun getWorksheetTitle(view: View, isLoadedSheetInfo: Boolean) {
+    fun getWorksheetTitle(view: View, sheetId: String, preorder_SheetNumber: Int, mail_order_SheetNumber: Int, grasping_Demand_SheetNumber: Int, isLoadedSheetInfo: Boolean) {
         viewModelScope.launch {
             if (isLoadedSheetInfo == false) {
                 _text_worksheetTitle.value = "없음"
+                _text_preorder_info_sheetTitle.value = "없음"
+                _text_mail_order_sheetTitle.value = "없음"
+                _text_grasping_demand_sheetTitle.value = "없음"
+
                 _image_loadedworksheetInfo.value = R.drawable.cancel_24dp
+                _image_preorder_info_sheet.value = R.drawable.cancel_24dp
+                _image_mail_order_sheet.value = R.drawable.cancel_24dp
+                _image_grasping_demand_sheet.value = R.drawable.cancel_24dp
                 Snackbar.make(view, "워크 시트 정보를 불러올 수 없습니다", Snackbar.LENGTH_LONG)
                     .show()
             }
             else {
-                val result = try {
+                _text_worksheetTitle.value = "공사 중..."
+                _image_loadedworksheetInfo.value = R.drawable.cancel_24dp
+
+                val result_preorder = try {
+                    PythonClass.getWorkSheet(sheetId, preorder_SheetNumber)
+                    Log.d("Debug", "preorder_SheetNumber : " + preorder_SheetNumber)
+                    Log.d("Debug", "preorder_SheetTitle : " +
+                            PythonClass.getVariable("worksheet")?.get("title").toString())
+                    PythonClass.getVariable("worksheet")?.get("title").toString()
+                } catch (e: PyException) {
+                    "null"
+                }
+
+                val result_mail_order = try {
+                    PythonClass.getWorkSheet(sheetId, mail_order_SheetNumber)
+                    Log.d("Debug", "mail_order_SheetNumber : " + mail_order_SheetNumber)
+                    Log.d("Debug", "mail_order_SheetTitle : " +
+                            PythonClass.getVariable("worksheet")?.get("title").toString())
+                    PythonClass.getVariable("worksheet")?.get("title").toString()
+                } catch (e: PyException) {
+                    "null"
+                }
+
+                val result_grasping_demand = try {
+                    PythonClass.getWorkSheet(sheetId, grasping_Demand_SheetNumber)
+                    Log.d("Debug", "grasping_Demand_SheetNumber : " + grasping_Demand_SheetNumber)
+                    Log.d("Debug", "grasping_Demand_SheetTitle : " +
+                            PythonClass.getVariable("worksheet")?.get("title").toString())
                     PythonClass.getVariable("worksheet")?.get("title").toString()
                 } catch (e: PyException) {
                     "null"
                 }
 
                 when {
-                    result != "null" -> {
-                        _text_worksheetTitle.value = result
-                        _image_loadedworksheetInfo.value = R.drawable.check_circle_24dp
+                    result_preorder != "null" -> {
+                        _text_preorder_info_sheetTitle.value = "선입금 시트 : " + result_preorder
+                        _image_preorder_info_sheet.value = R.drawable.check_circle_24dp
                     }
 
                     else -> {
-                        _text_worksheetTitle.value = "없음"
-                        _image_loadedworksheetInfo.value = R.drawable.cancel_24dp
+                        _text_preorder_info_sheetTitle.value = "없음"
+                        _image_preorder_info_sheet.value = R.drawable.cancel_24dp
+                        Snackbar.make(view, "워크 시트 정보를 불러올 수 없습니다", Snackbar.LENGTH_LONG)
+                            .show()
+                    }
+                }
+
+                when {
+                    result_mail_order != "null" -> {
+                        _text_mail_order_sheetTitle.value = "통판 시트 : " + result_mail_order
+                        _image_mail_order_sheet.value = R.drawable.check_circle_24dp
+                    }
+
+                    else -> {
+                        _text_mail_order_sheetTitle.value = "없음"
+                        _image_mail_order_sheet.value = R.drawable.cancel_24dp
+                        Snackbar.make(view, "워크 시트 정보를 불러올 수 없습니다", Snackbar.LENGTH_LONG)
+                            .show()
+                    }
+                }
+
+                when {
+                    result_grasping_demand != "null" -> {
+                        _text_grasping_demand_sheetTitle.value = "수요조사 시트 : " + result_grasping_demand
+                        _image_grasping_demand_sheet.value = R.drawable.check_circle_24dp
+                    }
+
+                    else -> {
+                        _text_grasping_demand_sheetTitle.value = "없음"
+                        _image_grasping_demand_sheet.value = R.drawable.cancel_24dp
                         Snackbar.make(view, "워크 시트 정보를 불러올 수 없습니다", Snackbar.LENGTH_LONG)
                             .show()
                     }
